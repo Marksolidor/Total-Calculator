55c9b211c179533fc1ae47aa5f78db45ae8aabee
Marksolidor <marco350sl@gmail.com>
2022-07-01 23:26:56 -0400

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 function myFunction() {
-    let nmr = document.querySelector("#input-1").value;
-    document.querySelector("#quantity").innerHTML = nmr;
-    let nmr2 = document.querySelector("#price");
-    let value = nmr2.options[nmr2.selectedIndex].value;
-    document.querySelector("#sum").innerHTML = Number(value) * Number(nmr);
-    let color = document.querySelector("#color");
-   document.querySelector(".circle").style.backgroundColor = color.value; 
+  let nmr = document.querySelector("#input-1").value;
+  document.querySelector("#quantity").innerHTML = nmr;
+  let nmr2 = document.querySelector("#price");
+  let value = nmr2.options[nmr2.selectedIndex].value;
+  document.querySelector("#sum").innerHTML = Number(value) * Number(nmr);
+  let color = document.querySelector("#color");
+  document.querySelector(".circle").style.backgroundColor = color.value;
 }
