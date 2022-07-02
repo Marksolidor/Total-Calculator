898f7ccf15d2a3c1b4a3a81dd32935c9cd5a6c4b
Marksolidor <marco350sl@gmail.com>
2022-07-01 23:02:38 -0400

    feat color change

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,9 @@ function myFunction() {
     let cantidadSpan = document.querySelector("#quantity");
     cantidadSpan.innerHTML = nmr;
     let nmr2 = document.querySelector("#price");
-    let value=nmr2.options[nmr2.selectedIndex].value;
+    let value = nmr2.options[nmr2.selectedIndex].value;
     document.querySelector("#sum").innerHTML = Number(value) * Number(nmr);
-    //let sum = Number(nmr) * Number(nmr2);
-   // let totalSpan = document.querySelector("#sum");
-    //totalSpan.innerHTML = sum;
-    console.log(nmr2);
-    console.log(nmr);
-    console.log(value)
+    let color = document.querySelector("#color");
+    let circle = color.options[color.selectedIndex].value;
+   document.querySelector(".circle").style.backgroundColor = color.value; 
 }
